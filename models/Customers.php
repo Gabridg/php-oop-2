14a651c87a3de1e336aaf4c733a6c67bd9e23b28
Gabridg <gabri99.del@gmail.com>
2022-09-12 17:18:21 +0200

    trying to define every class

--- models/Customers.php
+++ models/Customers.php
@@ -4,7 +4,29 @@ class Customers{
     public $email;
 
     public function __construct($username, $email){
-        
+        $this->setUserName($username);
+        $this->setEmail($email);
+    }
+
+
+    public function getUserName(){
+        return $this->username;
+    }
+
+    public function setUserName($username){
+        $this->username = $username;
+        return $this;
+
+    }
+    
+    public function getEmail(){
+        return $this->email;
+    }
+
+    public function setEmail($email){
+        $this->email = $email;
+        return $this;
+
     }
 }
 
