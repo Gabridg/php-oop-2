283fd4b0238718a63e579329857e71e6c9545260
Gabridg <gabri99.del@gmail.com>
2022-09-12 17:14:36 +0200

    fixed diagram and extended class

--- models/Logged_customer.php
+++ models/Logged_customer.php
@@ -6,9 +6,50 @@ class Logged_customer extends Customers{
     private $phone_number;
     private $discount;
 
-    public function __construct($username, $email, $first_name, $last_name, $address, $phone_number, $discount = 20)
-    parent::__construct($username, $email);
-    
+    public function __construct($username, $email, $first_name, $last_name, $address, $phone_number, $discount){
+        parent::__construct($username, $email);
+        $this->setFirstName($first_name);
+        $this->setLastName($last_name);
+        $this->setAddress($address);
+        $this->setPhoneNumber($phone_number);
+        $this->setDiscount($discount);
+    }
+
+    public function getFirstName(){
+        return $first_name;
+    }
+
+    public function setFirstName($size){
+        $this->size = $first_name;
+        return $this;
+    }
+     public function getLastName(){
+        return $this;
+    }
+
+    public function setLastName($last_name){
+        $this->last_name = $last_name;
+        return $this;
+    }
+    public function getAddress(){
+        return $this;
+    }
+
+    public function setAddress($address){
+        $this->address = $address;
+        return $this;
+    }
+    public function getPhoneNumber(){
+        return $this;
+    }
+
+    public function setPhoneNumber($phone_number){
+        $this->phone_number = $phone_number;
+        return $this;
+    }
+    public function getDiscount($perc){
+        return 20;
+    }    
 }
 
 
